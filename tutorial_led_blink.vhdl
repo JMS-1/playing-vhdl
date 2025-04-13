f910e3262af42e16a6b231109beea54951e4b594
Dr. Jochen Manns <dev-2@psimarron.net>
2025-04-13 10:45:27 +0200

    Playing version.

--- tutorial_led_blink.vhdl
+++ tutorial_led_blink.vhdl
@@ -1,108 +1,100 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
- 
-entity tutorial_led_blink is
-  port (
-    i_clock      : in  std_logic;
-    i_enable     : in  std_logic;
-    i_switch_1   : in  std_logic;
-    i_switch_2   : in  std_logic;
-    o_led_drive  : out std_logic
-    );
-end tutorial_led_blink;
- 
-architecture rtl of tutorial_led_blink is
- 
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY tutorial_led_blink IS
+  PORT (
+    i_clock : IN STD_LOGIC;
+    i_enable : IN STD_LOGIC;
+    i_switch_1 : IN STD_LOGIC;
+    i_switch_2 : IN STD_LOGIC;
+    o_led_drive : OUT STD_LOGIC
+  );
+END tutorial_led_blink;
+
+ARCHITECTURE behaviour OF tutorial_led_blink IS
+
   -- Constants to create the frequencies needed:
   -- Formula is: (25 MHz / 100 Hz * 50% duty cycle)
   -- So for 100 Hz: 25,000,000 / 100 * 0.5 = 125,000
-  constant c_CNT_100HZ : natural := 125000;
-  constant c_CNT_50HZ  : natural := 250000;
-  constant c_CNT_10HZ  : natural := 1250000;
-  constant c_CNT_1HZ   : natural := 12500000;
- 
+  CONSTANT c_CNT_100HZ : NATURAL := 125000;
+  CONSTANT c_CNT_50HZ : NATURAL := 250000;
+  CONSTANT c_CNT_10HZ : NATURAL := 1250000;
+  CONSTANT c_CNT_1HZ : NATURAL := 12500000;
+
   -- These signals will be the counters:
-  signal r_CNT_100HZ : natural range 0 to c_CNT_100HZ;
-  signal r_CNT_50HZ  : natural range 0 to c_CNT_50HZ;
-  signal r_CNT_10HZ  : natural range 0 to c_CNT_10HZ;
-  signal r_CNT_1HZ   : natural range 0 to c_CNT_1HZ;
-   
+  SIGNAL r_CNT_100HZ : NATURAL RANGE 0 TO c_CNT_100HZ;
+  SIGNAL r_CNT_50HZ : NATURAL RANGE 0 TO c_CNT_50HZ;
+  SIGNAL r_CNT_10HZ : NATURAL RANGE 0 TO c_CNT_10HZ;
+  SIGNAL r_CNT_1HZ : NATURAL RANGE 0 TO c_CNT_1HZ;
+
   -- These signals will toggle at the frequencies needed:
-  signal r_TOGGLE_100HZ : std_logic := '0';
-  signal r_TOGGLE_50HZ  : std_logic := '0';
-  signal r_TOGGLE_10HZ  : std_logic := '0';
-  signal r_TOGGLE_1HZ   : std_logic := '0';
- 
+  SIGNAL r_TOGGLE_100HZ : STD_LOGIC := '0';
+  SIGNAL r_TOGGLE_50HZ : STD_LOGIC := '0';
+  SIGNAL r_TOGGLE_10HZ : STD_LOGIC := '0';
+  SIGNAL r_TOGGLE_1HZ : STD_LOGIC := '0';
+
   -- One bit select wire.
-  signal w_LED_SELECT : std_logic;
-   
-begin
- 
+  SIGNAL w_LED_SELECT : STD_LOGIC;
+
+BEGIN
+
   -- All processes toggle a specific signal at a different frequency.
   -- They all run continuously even if the switches are
   -- not selecting their particular output.
-   
-  p_100_HZ : process (i_clock) is
-  begin
-    if rising_edge(i_clock) then
-      if r_CNT_100HZ = c_CNT_100HZ-1 then  -- -1, since counter starts at 0
-        r_TOGGLE_100HZ <= not r_TOGGLE_100HZ;
-        r_CNT_100HZ    <= 0;
-      else
+
+  p_100_HZ : PROCESS (i_clock) IS
+  BEGIN
+    IF rising_edge(i_clock) THEN
+      IF r_CNT_100HZ = c_CNT_100HZ - 1 THEN -- -1, since counter starts at 0
+        r_TOGGLE_100HZ <= NOT r_TOGGLE_100HZ;
+        r_CNT_100HZ <= 0;
+      ELSE
         r_CNT_100HZ <= r_CNT_100HZ + 1;
-      end if;
-    end if;
-  end process p_100_HZ;
- 
- 
-  p_50_HZ : process (i_clock) is
-  begin
-    if rising_edge(i_clock) then
-      if r_CNT_50HZ = c_CNT_50HZ-1 then  -- -1, since counter starts at 0
-        r_TOGGLE_50HZ <= not r_TOGGLE_50HZ;
-        r_CNT_50HZ    <= 0;
-      else
+      END IF;
+    END IF;
+  END PROCESS p_100_HZ;
+  p_50_HZ : PROCESS (i_clock) IS
+  BEGIN
+    IF rising_edge(i_clock) THEN
+      IF r_CNT_50HZ = c_CNT_50HZ - 1 THEN -- -1, since counter starts at 0
+        r_TOGGLE_50HZ <= NOT r_TOGGLE_50HZ;
+        r_CNT_50HZ <= 0;
+      ELSE
         r_CNT_50HZ <= r_CNT_50HZ + 1;
-      end if;
-    end if;
-  end process p_50_HZ;
- 
-   
-  p_10_HZ : process (i_clock) is
-  begin
-    if rising_edge(i_clock) then
-      if r_CNT_10HZ = c_CNT_10HZ-1 then  -- -1, since counter starts at 0
-        r_TOGGLE_10HZ <= not r_TOGGLE_10HZ;
-        r_CNT_10HZ    <= 0;
-      else
+      END IF;
+    END IF;
+  END PROCESS p_50_HZ;
+  p_10_HZ : PROCESS (i_clock) IS
+  BEGIN
+    IF rising_edge(i_clock) THEN
+      IF r_CNT_10HZ = c_CNT_10HZ - 1 THEN -- -1, since counter starts at 0
+        r_TOGGLE_10HZ <= NOT r_TOGGLE_10HZ;
+        r_CNT_10HZ <= 0;
+      ELSE
         r_CNT_10HZ <= r_CNT_10HZ + 1;
-      end if;
-    end if;
-  end process p_10_HZ;
- 
-   
-  p_1_HZ : process (i_clock) is
-  begin
-    if rising_edge(i_clock) then
-      if r_CNT_1HZ = c_CNT_1HZ-1 then  -- -1, since counter starts at 0
-        r_TOGGLE_1HZ <= not r_TOGGLE_1HZ;
-        r_CNT_1HZ    <= 0;
-      else
+      END IF;
+    END IF;
+  END PROCESS p_10_HZ;
+  p_1_HZ : PROCESS (i_clock) IS
+  BEGIN
+    IF rising_edge(i_clock) THEN
+      IF r_CNT_1HZ = c_CNT_1HZ - 1 THEN -- -1, since counter starts at 0
+        r_TOGGLE_1HZ <= NOT r_TOGGLE_1HZ;
+        r_CNT_1HZ <= 0;
+      ELSE
         r_CNT_1HZ <= r_CNT_1HZ + 1;
-      end if;
-    end if;
-  end process p_1_HZ;
- 
-   
+      END IF;
+    END IF;
+  END PROCESS p_1_HZ;
+
   -- Create a multiplexor based on switch inputs
-  w_LED_SELECT <= r_TOGGLE_100HZ when (i_switch_1 = '0' and i_switch_2 = '0') else
-                  r_TOGGLE_50HZ  when (i_switch_1 = '0' and i_switch_2 = '1') else
-                  r_TOGGLE_10HZ  when (i_switch_1 = '1' and i_switch_2 = '0') else
-                  r_TOGGLE_1HZ;
- 
-   
+  w_LED_SELECT <= r_TOGGLE_100HZ WHEN (i_switch_1 = '0' AND i_switch_2 = '0') ELSE
+    r_TOGGLE_50HZ WHEN (i_switch_1 = '0' AND i_switch_2 = '1') ELSE
+    r_TOGGLE_10HZ WHEN (i_switch_1 = '1' AND i_switch_2 = '0') ELSE
+    r_TOGGLE_1HZ;
+
   -- Only allow o_led_drive to drive when i_enable is high (and gate).
-  o_led_drive <= w_LED_SELECT and i_enable;
- 
-end rtl;
+  o_led_drive <= w_LED_SELECT AND i_enable;
+
+END behaviour;
---
+++ tutorial_led_blink_tb.vhdl
@@ -0,0 +1,54 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+-- A testbench has no ports.
+ENTITY tutorial_led_blink_tb IS
+END tutorial_led_blink_tb;
+
+ARCHITECTURE behaviour OF tutorial_led_blink_tb IS
+    -- Declaration of the component that will be instantiated.
+    COMPONENT tutorial_led_blink
+        PORT (
+            i_clock : IN STD_LOGIC;
+            i_enable : IN STD_LOGIC;
+            i_switch_1 : IN STD_LOGIC;
+            i_switch_2 : IN STD_LOGIC;
+            o_led_drive : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    -- Specifies which entity is bound with the component.
+    FOR cut_0 : tutorial_led_blink USE ENTITY work.tutorial_led_blink;
+    SIGNAL i_clock, i_enable, i_switch_1, i_switch_2, o_led_drive : STD_LOGIC;
+BEGIN
+    -- Component instantiation.
+    cut_0 : tutorial_led_blink PORT MAP(
+        i_clock => i_clock,
+        i_enable => i_enable,
+        i_switch_1 => i_switch_1,
+        i_switch_2 => i_switch_2,
+        o_led_drive => o_led_drive
+    );
+
+    PROCESS
+    BEGIN
+        i_enable <= '1';
+        i_switch_1 <= '0';
+        i_switch_2 <= '0';
+
+        FOR i IN 1 TO 2500000 LOOP
+            WAIT FOR 40 ns;
+
+            i_clock <= '0';
+
+            WAIT FOR 40 ns;
+
+            i_clock <= '1';
+        END LOOP;
+
+        ASSERT false REPORT "end of test" SEVERITY note;
+
+        WAIT;
+    END PROCESS;
+END behaviour;
