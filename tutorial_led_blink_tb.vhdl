2f473ee2769430bd51786d3126e3de106062b0e3
Dr. Jochen Manns <dev-2@psimarron.net>
2025-04-15 00:30:11 +0200

    Simplified test bench - only generate clock.

--- tutorial_led_blink_tb.vhdl
+++ tutorial_led_blink_tb.vhdl
@@ -20,7 +20,11 @@ ARCHITECTURE behaviour OF tutorial_led_blink_tb IS
 
     -- Specifies which entity is bound with the component.
     FOR cut_0 : tutorial_led_blink USE ENTITY work.tutorial_led_blink;
-    SIGNAL i_clock, i_enable, i_switch_1, i_switch_2, o_led_drive : STD_LOGIC;
+    SIGNAL i_clock : STD_LOGIC := '0';
+    SIGNAL i_enable : STD_LOGIC := '1';
+    SIGNAL i_switch_1 : STD_LOGIC := '0';
+    SIGNAL i_switch_2 : STD_LOGIC := '1';
+    SIGNAL o_led_drive : STD_LOGIC;
 BEGIN
     -- Component instantiation.
     cut_0 : tutorial_led_blink PORT MAP(
@@ -33,22 +37,12 @@ BEGIN
 
     PROCESS
     BEGIN
-        i_enable <= '1';
-        i_switch_1 <= '0';
-        i_switch_2 <= '1';
+        WAIT FOR 20 ns;
 
-        FOR i IN 1 TO 500000 LOOP
-            WAIT FOR 20 ns;
-
+        IF i_clock = '1' THEN
             i_clock <= '0';
-
-            WAIT FOR 20 ns;
-
+        ELSE
             i_clock <= '1';
-        END LOOP;
-
-        ASSERT false REPORT "end of test" SEVERITY note;
-
-        WAIT;
+        END IF;
     END PROCESS;
 END behaviour;
